d020d74d7ef2e85ce37bce4566681a65cd627334
Sébastien David <sebastien-david@outlook.com>
2017-10-07 11:18:06 +0200

    moment bundle optimization

--- config/base.js
+++ config/base.js
@@ -55,6 +55,7 @@ module.exports = function baseConfig() {
         name: 'manifest',
         minChunks: Infinity,
       }),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     ],
     stats: {
       assets: true,
